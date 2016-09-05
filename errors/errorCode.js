237ce01bf257acedce7797a55ae20b500a8a1928
Lei,HUANG <mrsatangel@gmail.com>
2016-09-05 21:17:18 +0800

    Join table sjd_member and sjd_ucenter_member

--- errors/errorCode.js
+++ errors/errorCode.js
@@ -1,9 +1,9 @@
 module.exports = {
 	"ENULLOID": {"message": "微信ID为空", "status": "400"},
 	"ENULLUID": {"message": "用户ID为空", "status": "404"},
-	"EOIDEXISTS": {"message": "微信ID已经绑定", "status": "400"},
+	"EOIDEXISTS": {"message": "微信ID已经绑定", "status": "200"},
 	"EREDISERR": {"message": "服务器错误！", "status": "500"},
-	"ENORES": {"message": "未找到用户信息！", "status": "404"},
+	"ENORES": {"message": "未找到用户信息！", "status": "200"},
 	"ENULLREQ": {"message": "请求错误！", "status": "400"},
 	"EINTERNAL": {"message": "服务器错误！", "status": "500"},
 	"SUCCESS": {"message": "请求成功！", "status": "200"}
