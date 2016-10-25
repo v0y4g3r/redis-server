00172cdc8ae6020e7c600081c8fcac11dd647615
Lei,HUANG <mrsatangel@gmail.com>
2016-10-25 11:29:31 +0800

    fix bugs

--- routes/register.js
+++ routes/register.js
@@ -15,30 +15,30 @@ var thisClient;
  * @param {string} request.body.field
  */
 router.post('/', function (request, response, next) {
-	if (!thisClient) return next(errorCode.EREDISERR);
-	if (!request.body) return next(errorCode.ENULLREQ);
-	
-	var sjdMemberInstance = utils.partialCast(sjdMember_schema.concat(sjdUcenterMember_schema), request.body);//trim useless attr(in case)
-	var uid = sjdMemberInstance['uid']; //get mobile phone number
-	if (!uid) return next(errorCode.ENULLUID);
+  if (!thisClient) return next(errorCode.EREDISERR);
+  if (!request.body) return next(errorCode.ENULLREQ);
 
-	console.log('redis db:'+config.redis);
-	thisClient.hmset('sjd_member:' + uid, sjdMemberInstance, (function (uid) {
-		return function (err, res) {
-			if (err) {
-				console.log('Set entry: sjd_member:' + uid + ' error!\n' + err.stack);
-				return next(errorCode.EREDISERR);
-			} else {
-				console.log('Set entry sjd_member:' + uid + ' success.');
-				return next(errorCode.SUCCESS);
-			}
-		}
-	})(uid));
+  var sjdMemberInstance = utils.partialCast(sjdMember_schema.concat(sjdUcenterMember_schema), request.body);//trim useless attr(in case)
+  var uid = sjdMemberInstance['uid']; //get mobile phone number
+  if (!uid) return next(errorCode.ENULLUID);
+
+  console.log('redis db:' + config.redis);
+  thisClient.hmset('sjd_member:' + uid, sjdMemberInstance, (function (uid) {
+    return function (err, res) {
+      if (err) {
+        console.log('Set entry: sjd_member:' + uid + ' error!\n' + err.stack);
+        return next(errorCode.EREDISERR);
+      } else {
+        console.log('Set entry sjd_member:' + uid + ' success.');
+        return next(errorCode.SUCCESS);
+      }
+    }
+  })(uid));
 });
 
 module.exports = {
-	'router': router,
-	'setClient': function (inClient) {
-		thisClient = inClient;
-	}
+  'router': router,
+  'setClient': function (inClient) {
+    thisClient = inClient;
+  }
 };
